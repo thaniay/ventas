f0d9570f76bbaa5898e06b2f65f8c90e74ceadab
Thania Yanez <yorelyyanezguevara@gmail.com>
2021-02-09 04:34:05 -0600

    Agregando frace motivadora

--- Ventas/Ventas/Form1.cs
+++ Ventas/Ventas/Form1.cs
@@ -19,7 +19,7 @@ namespace Ventas
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Hola Mundo");
+            MessageBox.Show("Vuela alto y no te detengas...");
         }
     }
 }
